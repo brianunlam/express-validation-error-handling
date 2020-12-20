44a8eb20f26b5835dfdb593e36264444af686385
Brian Alonso <brian.alonso.a@gmail.com>
2020-12-20 20:22:19 -0300

    Refactor async middleware

--- src/middlewares/asyncMiddleware.js
+++ src/middlewares/asyncMiddleware.js
@@ -1,12 +1,14 @@
 const boom = require('@hapi/boom');
 
-const asyncMiddleware = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => {
+const asyncMiddleware = (fn) => async (req, res, next) => {
+  try {
+     await fn(req, res, next);
+  } catch (err) {
     if (!err.isBoom) {
       return next(boom.badImplementation(err));
     }
     next(err);
-  });
+  }
 };
 
 module.exports = asyncMiddleware;
